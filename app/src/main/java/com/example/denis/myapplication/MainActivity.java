9efc57061cb5ee0d090fac208089bd1fc8193090
REDDII <dhnotes@gmail.com>
2017-02-26 23:05:32 +0100

    v0.3

--- app/src/main/java/com/example/denis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/denis/myapplication/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.denis.myapplication;
 //N'oubliez pas de déclarer le bon package dans lequel se trouve le fichier !
-// nouvelle version
+// nouvelle version II
 
 import android.app.Activity;
 
